89bdcb908d2e01fea5ae5e62d7dc512921068fef
merci <1978427972@qq.com>
2021-05-26 23:30:57 +0800

    mark

--- src/platforms/web/runtime/patch.js
+++ src/platforms/web/runtime/patch.js
@@ -8,5 +8,7 @@ import platformModules from 'web/runtime/modules/index'
 // the directive module should be applied last, after all
 // built-in modules have been applied.
 const modules = platformModules.concat(baseModules)
-
+// nodeOps是一些dom创建、删除、插入元素的方法
+// baseModules 是ref和directives
+// platformModules 是关于属性，class，events，domProps，style，transition的创建和更新方法
 export const patch: Function = createPatchFunction({ nodeOps, modules })
